854c7a1876d45cbb04fbdbdfeb98c621b761f492
Hailey Thomas <haileyraebeauty@gmail.com>
2022-01-29 19:29:59 -0700

    finish header and start about me

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import headerImage from "./assets/cover/header.png";
 import About from "./components/About";
 import Nav from "./components/Nav";
 
@@ -7,7 +8,14 @@ function App() {
     <div>
       <Nav></Nav>
       <main className="container">
-        <About></About>
+        <section className="columns">
+          <div className="column">
+            <img src={headerImage} className="p-1" alt="header" />
+            <div className="columns green mr-1 ml-1 p-3">
+              <About></About>
+            </div>
+          </div>
+        </section>
       </main>
     </div>
   );
--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,20 +1,14 @@
 import React from "react";
-import headerImage from "../../assets/cover/header.png";
 import headshot from "../../assets/images/headshot.jpg";
 
 function About() {
   return (
-    <section id="about-me" className="columns">
+    <section className="columns">
       <div className="column">
-        <img src={headerImage}  className="p-1" alt="header" />
-        <div className="columns green mr-1 ml-1 p-3">
-          <div className="column">
-            <img src={headshot} alt="headshot of developer" />
-          </div>
-          <div className="column">
-            <h1 id="about">About Me</h1>
-          </div>
-        </div>
+        <img src={headshot} alt="headshot of developer" />
+      </div>
+      <div className="column">
+        <h1 id="about" className="has-text-white is-size-3">About Me</h1>
       </div>
     </section>
   );
