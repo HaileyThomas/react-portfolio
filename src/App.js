bb2c882bb33ba28f0c96871753c63b834c38af91
Hailey Thomas <haileyraebeauty@gmail.com>
2022-01-31 14:02:51 -0700

    finish footer

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Nav from "./components/Nav";
 // import Portfolio from "./components/Portfolio";
 // import Contact from "./components/Contact";
 import Resume from "./components/Resume";
+import Icon from "@mdi/react";
+import { mdiGithub } from "@mdi/js";
+import { mdiLinkedin } from "@mdi/js";
 
 function App() {
   return (
@@ -19,7 +22,33 @@ function App() {
             <div className="columns green mr-1 ml-1 p-3">
               <Resume></Resume>
             </div>
-            <img src={footerImage} alt="footer" />
+            <div
+              className="columns mr-1 ml-1 green"
+              style={{ backgroundImage: `url(${footerImage})` }}
+            >
+              <div className="column is-four-fifths is-offset-1">
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <p className="has-text-white has-text-centered is-size-4">
+                  <a href="mailto:haileyraebeauty@gmail.com">
+                    <span className="icon has-text-white is-large is-size-1">
+                      <Icon path={mdiLinkedin} />
+                    </span>
+                  </a>
+                  <a href="https://github.com/HaileyThomas" target="blank">
+                    <span className="icon has-text-white is-large is-size-1">
+                      <Icon path={mdiGithub} />
+                    </span>
+                  </a>
+                </p>
+                <p className="is-size-3 has-text-white">
+                  &copy; Hailey Thomas, 2022. Thank you so much for visiting ❤️
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </main>
